f678bb2261e44bec716f7d1bf6bce84eb664a4e0
AlexNatan <148141202+AlexNatan@users.noreply.github.com>
2024-01-23 10:08:51 -0300

    Add files via upload

---
+++ projetosenac.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23-Jan-2024 às 14:03
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projetosenac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `hentrada` time(6) NOT NULL,
+  `UC` varchar(50) NOT NULL,
+  `sala` varchar(50) NOT NULL,
+  `docente` varchar(50) NOT NULL,
+  `local` varchar(50) NOT NULL,
+  `hsaida` time(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `dadosaluno`
+--
+
+CREATE TABLE `dadosaluno` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(50) NOT NULL,
+  `oemissor` varchar(50) NOT NULL,
+  `ufdocumento` varchar(50) NOT NULL,
+  `dnascimento` date NOT NULL,
+  `sexo` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefone` int(50) NOT NULL,
+  `nacionalidade` varchar(50) NOT NULL,
+  `ufaluno` varchar(50) NOT NULL,
+  `cidadealuno` varchar(50) NOT NULL,
+  `nomemãe` varchar(50) NOT NULL,
+  `nomepai` varchar(50) NOT NULL,
+  `estadocivil` varchar(50) NOT NULL,
+  `deficiencia` varchar(50) NOT NULL,
+  `cep` varchar(50) NOT NULL,
+  `numero` int(50) NOT NULL,
+  `complemento` varchar(50) NOT NULL,
+  `logradouro` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `ufendereco` varchar(50) NOT NULL,
+  `cidadeendereco` varchar(50) NOT NULL,
+  `escolaridade` varchar(50) NOT NULL,
+  `tinstituicao` varchar(50) NOT NULL,
+  `formacao` varchar(50) NOT NULL,
+  `curso` varchar(50) NOT NULL,
+  `ufcurso` varchar(50) NOT NULL,
+  `cidadecurso` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `financeiro`
+--
+
+CREATE TABLE `financeiro` (
+  `id` int(11) NOT NULL,
+  `situacao` varchar(50) NOT NULL,
+  `financiador` varchar(50) NOT NULL,
+  `vencimento` date NOT NULL,
+  `fpagamento` varchar(50) NOT NULL,
+  `parcela` int(50) NOT NULL,
+  `valor` varchar(50) NOT NULL,
+  `acoes` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `dadosaluno`
+--
+ALTER TABLE `dadosaluno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `financeiro`
+--
+ALTER TABLE `financeiro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `dadosaluno`
+--
+ALTER TABLE `dadosaluno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `financeiro`
+--
+ALTER TABLE `financeiro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
